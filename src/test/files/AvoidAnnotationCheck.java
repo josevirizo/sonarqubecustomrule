a082e2749d5e5869344c1e6dd22d6afe323f1708
Jose Yañez Perez <jyanez@orienteed.com>
2018-08-24 11:02:24 +0200

    First code commit

---
+++ src/test/files/AvoidAnnotationCheck.java
@@ -0,0 +1,21 @@
+import org.junit.BeforeClass;
+
+/**
+ *This file is the sample code against we run our unit test.
+ *It is placed src/test/files in order to not be part of the maven compilation.
+ **/
+class AvoidAnnotationCheck {
+
+  int aField;
+
+  @MyAnnotation
+  public void aMethod() {
+
+  }
+
+  @BeforeClass
+  public void aMethod() {
+
+  }
+
+}
